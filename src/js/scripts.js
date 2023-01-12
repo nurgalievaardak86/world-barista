54ebd3c97439f1db1f51c704353df2914ac1ead8
Ардак Нургалиева <nurgalievaardak86@gmail.com>
2023-01-12 16:17:12 +0600

    Ardak Nurgalieva final project

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,14 +1,59 @@
-window.onload = function () {
+function closeup(){
+    setTimeout(function(){
+        $('#exampleModal').modal('hide')
+    }, 300)
+}
 
-    var el = document.createElement("div");
-    el.innerHTML = "RABBIT TEST ";
-    var div = document.getElementById("headline");
-    insertAfter(div, el);
+let slideIndex = 1;
+showSlides(slideIndex);
 
-    /* FUNCTIONS */
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
 
-    // Inserts newNode after referenceNode
-    function insertAfter(referenceNode, newNode) {
-        referenceNode.parentNode.insertBefore(newNode, referenceNode.nextSibling);
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("slides");
+  let dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}    
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";  
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";  
+  dots[slideIndex-1].className += " active";
+}
+
+const search = () => {
+    const searchBox = document.querySelector('.form__search').value.toUpperCase();
+    const searchItems = document.querySelector('.cards');
+    const search = document.querySelector('.cards-box');
+    const pname = document.querySelector('.cards__title');
+
+    for(let i = 0; i < pname.length; i++) {
+        let match = search[i].querySelector('.cards__title')[0];
+
+        if(match) {
+            let textValue = match.textContent || match.innerHTML
+
+            if(textValue.toUpperCase().indexOf(searchBox) > - 1) {
+                search[i].style.display = "";
+            } else{
+                search[i].style.display = "none";
+            }
+        }
     }
-};
+}
+
+
+
+
+
+  
